70d6a8306e47f1c777269df32e84dc0b6344b624
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2025-02-25 15:14:21 +0100

    Add project

---
+++ src/test/java/org/allure/junit_selenium_screenshots/ScreenshotTest.java
@@ -0,0 +1,36 @@
+package org.allurereport.junit_selenium_screenshots;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ScreenshotTest {
+
+    private static ChromeDriver driver;
+
+    @RegisterExtension
+    ScreenshotOnFailRule watcher = new ScreenshotOnFailRule(driver, "screenshots");
+
+    @BeforeAll
+    public static void setupDriver() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void lambdaStepTest() {
+        driver.get("https://en.wikipedia.org/wiki/Software_testing");
+
+        assertFalse(driver.getTitle().contains("Software testing"));
+    }
+
+    @AfterAll
+    public static void teardownDriver() {
+        driver.quit();
+    }
+
+
+}
