029902b6dc895d5a557fc1c2e6e59d1c54dbadf8
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-03-06 20:09:06 +0700

    adapt new example for guide

--- src/test/java/org/allurereport/guide/ScreenshotTest.java
+++ src/test/java/org/allurereport/guide/ScreenshotTest.java
@@ -3,18 +3,36 @@ package org.allurereport.guide;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 class ScreenshotTest {
 
     @RegisterExtension
-    WebdriverExtension watcher = new WebdriverExtension();
+    WebdriverExtension webDriverManager = new WebdriverExtension();
+
+    @RegisterExtension
+    AutoScreenshotExtension screenshotManager = new AutoScreenshotExtension(
+            c -> this.webDriverManager.getWebDriver(c));
 
     @Test
-    void lambdaStepTest(WebDriver driver) {
-        driver.get("https://allurereport.org/");
+    void allureCommunityTest(WebDriver webDriver) {
+        webDriver.get("https://allurereport.org/");
 
-        Assertions.assertEquals("Allure Report", driver.getTitle());
+        final WebElement element = webDriver
+                .findElement(By.xpath("//*[contains(text(), 'Trusted by the Community')]"));
+
+        Assertions.assertNotNull(element);
+
+        final WebElement container = element.findElement(By.xpath("./../.."));
+        ScreenshotUtils.attachElementScreenshot(container, "Allure Report comminuty.png");
     }
 
+    @Test
+    void allureTitleFailedTest(WebDriver webDriver) {
+        webDriver.get("https://allurereport.org/");
+
+        Assertions.assertEquals("Allure Report", webDriver.getTitle());
+    }
 }
--- src/test/java/org/allurereport/guide/WebdriverExtension.java
+++ src/test/java/org/allurereport/guide/WebdriverExtension.java
@@ -1,36 +1,30 @@
 package org.allurereport.guide;
 
-import io.qameta.allure.Allure;
+import java.util.Objects;
+
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.ByteArrayInputStream;
-import java.util.Objects;
-
-
-public class WebdriverExtension implements TestExecutionExceptionHandler, ParameterResolver, BeforeEachCallback, AfterEachCallback {
+public class WebdriverExtension implements ParameterResolver, BeforeEachCallback, AfterEachCallback {
 
     static final ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(WebdriverExtension.class);
     public static final String WEBDRIVER = "webdriver";
 
     @Override
     public boolean supportsParameter(final ParameterContext parameterContext,
-                                     final ExtensionContext extensionContext) throws ParameterResolutionException {
+            final ExtensionContext extensionContext) throws ParameterResolutionException {
         return parameterContext.getParameter().getType().isAssignableFrom(WebDriver.class);
     }
 
     @Override
     public WebDriver resolveParameter(final ParameterContext parameterContext,
-                                      final ExtensionContext extensionContext) throws ParameterResolutionException {
+            final ExtensionContext extensionContext) throws ParameterResolutionException {
         final ExtensionContext.Store store = extensionContext.getStore(NAMESPACE);
         return store.get(WEBDRIVER, WebDriver.class);
     }
@@ -50,16 +44,9 @@ public class WebdriverExtension implements TestExecutionExceptionHandler, Parame
         }
     }
 
-    @Override
-    public void handleTestExecutionException(final ExtensionContext context, final Throwable throwable) throws Throwable {
+    public WebDriver getWebDriver(ExtensionContext context) {
         final ExtensionContext.Store store = context.getStore(NAMESPACE);
 
-        final WebDriver webDriver = store.get(WEBDRIVER, WebDriver.class);
-        if (Objects.nonNull(webDriver)) {
-            byte[] screenshot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
-            Allure.attachment("Screenshot on failure (" + throwable.getClass().getSimpleName() + ")", new ByteArrayInputStream(screenshot));
-        }
-
-        throw throwable;
+        return store.get(WEBDRIVER, WebDriver.class);
     }
 }
